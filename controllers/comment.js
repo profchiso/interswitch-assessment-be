3ab591a5bdbeaaa40c992c364383797f99997697
Chinedu Okorie <okoriechinedusunday@gmail.com>
2024-07-04 12:09:47 +0100

    Chore: refactor comment controller

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,3 +1,4 @@
+const { isValidObjectId } = require("mongoose");
 const {
   getOne,
   getAll,
@@ -10,19 +11,62 @@ const {
 } = require("../utils");
 
 const { Comment } = require("../models/comment");
+const { Post } = require("../models/post");
+
+const populate = {
+  required: true,
+  field: "user",
+  columns: "name -_id",
+};
 
 exports.getAllComments = async (req, res) => {
   try {
-    getAll(req, res, Comment);
+    const data = await getAll(
+      req,
+      res,
+      Comment,
+      ["__v"],
+      populate,
+      "Comments Fetched successfully"
+    );
+
+    res.status(STATUS_CODES.OK).json({
+      statusCode: STATUS_CODES.OK,
+      responseText: RESPONSE_TEXT.SUCCESS,
+      data,
+    });
   } catch (error) {
     console.log(error);
+    return res.status(STATUS_CODES.BAD_REQUEST).json({
+      statusCode: STATUS_CODES.BAD_REQUEST,
+      responseText: RESPONSE_TEXT.FAIL,
+      errors: [{ msg: error.message || "something went wrong" }],
+    });
   }
 };
 exports.getAComment = async (req, res) => {
   try {
-    getOne(req, res, Comment);
+    const data = await getOne(
+      req,
+      res,
+      Comment,
+      ["__v"],
+      populate,
+      "Comment Fetched successfully"
+    );
+
+    res.status(STATUS_CODES.OK).json({
+      statusCode: STATUS_CODES.OK,
+      responseText: RESPONSE_TEXT.SUCCESS,
+      data,
+    });
   } catch (error) {
     console.log(error);
+    return res.status(STATUS_CODES.BAD_REQUEST).json({
+      statusCode: STATUS_CODES.BAD_REQUEST,
+      responseText: RESPONSE_TEXT.FAIL,
+      errors: [{ msg: error.message || "something went wrong" }],
+    });
   }
 };
 
@@ -30,20 +74,46 @@ exports.createComment = async (req, res) => {
   try {
     await validationCheck(req, res);
     req.body.user = req.user.id;
-    const created = await createDocument(
+
+    //check if req.body.post is a valid object
+    if (!isValidObjectId(req.body.post)) {
+      return res.status(STATUS_CODES.BAD_REQUEST).json({
+        statusCode: STATUS_CODES.BAD_REQUEST,
+        responseText: RESPONSE_TEXT.FAIL,
+        errors: [{ msg: "Invalid objectId passed" }],
+      });
+    }
+
+    //check if post exists
+    const post = await Post.findById(req.body.post);
+    if (!post) {
+      return res.status(STATUS_CODES.NOT_FOUND).json({
+        statusCode: STATUS_CODES.NOT_FOUND,
+        responseText: RESPONSE_TEXT.FAIL,
+        errors: [{ msg: "Post not found" }],
+      });
+    }
+    const data = await createDocument(
       req,
       res,
       Comment,
       {},
       "Comment created successfully"
     );
+    post.comments.push(data.resource._id);
+    await post.save();
     res.status(STATUS_CODES.CREATED).json({
       statusCode: STATUS_CODES.CREATED,
       responseText: RESPONSE_TEXT.SUCCESS,
-      data: created,
+      data,
     });
   } catch (error) {
     console.log(error);
+    return res.status(STATUS_CODES.BAD_REQUEST).json({
+      statusCode: STATUS_CODES.BAD_REQUEST,
+      responseText: RESPONSE_TEXT.FAIL,
+      errors: [{ msg: error.message || "something went wrong" }],
+    });
   }
 };
 
@@ -60,9 +130,26 @@ exports.updateComment = async (req, res) => {
         errors: [{ msg: "You can only update a Comment that belongs to you" }],
       });
     }
-    updateDocument(req, res, Comment, "Comment updated successfully");
+
+    const data = await updateDocument(
+      req,
+      res,
+      Comment,
+      "Comment updated successfully"
+    );
+
+    res.status(STATUS_CODES.OK).json({
+      statusCode: STATUS_CODES.OK,
+      responseText: RESPONSE_TEXT.SUCCESS,
+      data,
+    });
   } catch (error) {
     console.log(error);
+    return res.status(STATUS_CODES.BAD_REQUEST).json({
+      statusCode: STATUS_CODES.BAD_REQUEST,
+      responseText: RESPONSE_TEXT.FAIL,
+      errors: [{ msg: error.message || "something went wrong" }],
+    });
   }
 };
 
@@ -79,8 +166,24 @@ exports.deleteComment = async (req, res) => {
         errors: [{ msg: "You can only update a Comment that belongs to you" }],
       });
     }
-    deleteDocument(req, res, Comment, "Comment deleted successfully");
+
+    const data = await deleteDocument(
+      req,
+      res,
+      Comment,
+      "Comment deleted successfully"
+    );
+    res.status(STATUS_CODES.OK).json({
+      statusCode: STATUS_CODES.OK,
+      responseText: RESPONSE_TEXT.SUCCESS,
+      data,
+    });
   } catch (error) {
     console.log(error);
+    return res.status(STATUS_CODES.BAD_REQUEST).json({
+      statusCode: STATUS_CODES.BAD_REQUEST,
+      responseText: RESPONSE_TEXT.FAIL,
+      errors: [{ msg: error.message || "something went wrong" }],
+    });
   }
 };
